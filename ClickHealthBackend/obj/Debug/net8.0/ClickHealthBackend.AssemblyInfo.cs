b7953dcb4aaf70ef517143a0059be1a2283c156e
sahana <giberusahanagn@gmail.com>
2025-10-15 14:43:37 +0530

    Added all folders using .gitkeep

--- ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
+++ ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc698230964cbb1d8633a3e6ec1216211939a412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8277dc8d40cec344868d83ae8a494c8d56dd016c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

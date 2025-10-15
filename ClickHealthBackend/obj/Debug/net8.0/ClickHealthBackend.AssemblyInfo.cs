9248976b054d0ab76a9b5ee9fb99b8668fcb3fc7
Divya-venu <gvdivyashree@gmail.com>
2025-10-15 17:35:24 +0530

    Save work before merging

--- ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
+++ ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074fed55860d2060de3864bc03a02c7f8158e6dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a263f856c62424d05e6ebaae645dbef5933e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

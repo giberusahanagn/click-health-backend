15c0e6d6d1209bfba885f53aa34fde8f0785a25d
Divya-venu <gvdivyashree@gmail.com>
2025-10-17 10:16:25 +0530

    saved

--- ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
+++ ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9248976b054d0ab76a9b5ee9fb99b8668fcb3fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9f8c96bb04c4970f80f240336f9400be3dd005")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

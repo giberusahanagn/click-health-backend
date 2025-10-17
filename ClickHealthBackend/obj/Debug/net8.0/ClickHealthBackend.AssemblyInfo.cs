14619afd7ee61fb46c3751dee49a67ffb2fa4727
Divya-venu <gvdivyashree@gmail.com>
2025-10-17 10:17:52 +0530

    added file to integration

--- ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
+++ ClickHealthBackend/obj/Debug/net8.0/ClickHealthBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fc9a2e5a49f4a3d118e5a1926439a7ba5e9116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e93b91021c9bc66ee9d39d20cb8a8025522f3c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClickHealthBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
